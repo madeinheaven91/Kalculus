fe37285d5de058fb73b71a8e9b8f803b91196564
DmitryHudrich <dimaprudnikov9@gmail.com>
2023-07-07 12:17:17 +0300

    Remove multiply matrix by scalar

--- Kalculus/LinearAlgebra/Matrices/Evaluate.cs
+++ Kalculus/LinearAlgebra/Matrices/Evaluate.cs
@@ -62,5 +62,53 @@ namespace Kalculus.LinearAlgebra.Matrices
             }
             return new Matrix(minor);
         }
+
+        public static Matrix MultiplyByScalar(double scalar, Matrix matrix)
+        {
+            int rows = matrix.Rows;
+            int cols = matrix.Columns;
+
+            double[,] result = new double[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    result[i, j] = scalar * matrix[i, j];
+                }
+            }
+
+            return new Matrix(result);
+        }
+
+        public static Matrix MultiplyMatrices(Matrix leftMatrix, Matrix rightMatrix) 
+        {
+            if (leftMatrix.Columns != rightMatrix.Rows)
+            {
+                throw new ArgumentException("Invalid matrix dimensions");
+            }
+
+            int rows = leftMatrix.Rows;
+            int cols = rightMatrix.Columns;
+            int commonLength = leftMatrix.Columns;
+
+            double[,] result = new double[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < commonLength; k++)
+                    {
+                        sum += leftMatrix[i, k] * rightMatrix[k, j];
+                    }
+                    result[i, j] = sum;
+                }
+            }
+
+            return new Matrix(result);
+        }
+
     }
 }
--- Kalculus/LinearAlgebra/Matrices/MatrixOperators.cs
+++ Kalculus/LinearAlgebra/Matrices/MatrixOperators.cs
@@ -5,6 +5,7 @@ using System.Net.Http.Headers;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using Kalculus.LinearAlgebra.Matrices;
 
 namespace Kalculus.LinearAlgebra.Matrices
 {
@@ -12,57 +13,12 @@ namespace Kalculus.LinearAlgebra.Matrices
     {
         public static Matrix operator *(double scalar, Matrix matrix)
         {
-            return ScalarMultiply(scalar, matrix);
-        }
-        public static Matrix operator *(Matrix matrix, double scalar)
-        {
-            return ScalarMultiply(scalar, matrix);
-        }
-        private static Matrix ScalarMultiply (double scalar, Matrix matrix)
-        {
-            int rows = matrix.Rows;
-            int cols = matrix.Columns;
-
-            double[,] result = new double[rows, cols];
-
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < cols; j++)
-                {
-                    result[i, j] = scalar * matrix[i, j];
-                }
-            }
-
-            return new Matrix(result);
+            return Evaluate.MultiplyByScalar(scalar, matrix);
         }
 
         public static Matrix operator*(Matrix leftMatrix, Matrix rightMatrix)
         {
-            if (leftMatrix.Columns != rightMatrix.Rows)
-            {
-                throw new ArgumentException("Invalid matrix dimensions");
-            }
-
-            int rows = leftMatrix.Rows;
-            int cols = rightMatrix.Columns;
-            int commonLength = leftMatrix.Columns;
-
-            double[,] result = new double[rows, cols];
-
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < cols; j++)
-                {
-                    double sum = 0;
-                    for (int k = 0; k < commonLength; k++)
-                    {
-                        sum += leftMatrix[i, k] * rightMatrix[k, j];
-                    }
-                    result[i, j] = sum;
-                }
-            }
-
-            return new Matrix(result);
+            return Evaluate.MultiplyMatrices(leftMatrix, rightMatrix);
         }
     }
 }
