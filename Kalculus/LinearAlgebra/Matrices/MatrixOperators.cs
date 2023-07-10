319b9d705ed3480d2f6c688996a5bff12ec21b5a
DmitryHudrich <dimaprudnikov9@gmail.com>
2023-07-10 16:24:32 +0300

    add dev project for debug

--- Kalculus/LinearAlgebra/Matrices/MatrixOperators.cs
+++ Kalculus/LinearAlgebra/Matrices/MatrixOperators.cs
@@ -14,20 +14,7 @@
 
         public static Matrix operator+(Matrix leftMatrix, Matrix rightMatrix)
         {
-            if (leftMatrix.Columns != rightMatrix.Columns || leftMatrix.Rows != rightMatrix.Columns)
-                throw new ArgumentException("The matrices must have same size");
-
-            int cols = leftMatrix.Columns;
-            int rows = rightMatrix.Rows;
-            double[,] matrix = new double[rows, cols];
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < cols; j++)
-                {
-                    matrix[i,j] = leftMatrix[i,j] + rightMatrix[i,j];
-                }
-            }
-            return new Matrix(matrix);
+            return Evaluate.MatrixSubtraction(leftMatrix, rightMatrix);
         }
 
     }
